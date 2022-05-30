8c3b44ea170962997d253c039966d63df105a3d5
ThePiii <yf13166387172@gmail.com>
2022-05-30 17:40:49 +0800

    模型细微修正

--- predict.py
+++ predict.py
@@ -2,12 +2,11 @@ import cv2
 import numpy as np
 from crnn_model import crnn
 import torch
-from Image import Image
 
-char2num_dict = {'0': 0, '1': 1, '2':2, '3': 3,
-                '4': 4, '5': 5, '6': 6, '7': 7,
-                '8': 8, '9': 9, '_': 10}
-num2char_dict = {value : key for key, value in char2num_dict.items()}
+char2num_dict = {'0': 0, '1': 1, '2': 2, '3': 3,
+                 '4': 4, '5': 5, '6': 6, '7': 7,
+                 '8': 8, '9': 9, '_': 10}
+num2char_dict = {value: key for key, value in char2num_dict.items()}
 
 model_dir = "model/model.pth"
 
@@ -26,6 +25,7 @@ class GreedyCTCDecoder(torch.nn.Module):
         Returns:
           List[str]: The resulting transcript
         """
+        # zheli
         indices = torch.argmax(emission, dim=-1)  # [num_seq,]
         indices = torch.unique_consecutive(indices, dim=-1)
         indices = [int(i) for i in indices if i != self.blank]
@@ -33,13 +33,13 @@ class GreedyCTCDecoder(torch.nn.Module):
         return joined
 
 
-def single_recognition(img,model_dir):
+def single_recognition(img, model_dir):
     '''
     输入的是np array
     '''
     # 对读入图片做一些处理
-    img = cv2.resize(img,(256,32))
-    img = img / 255.0 * 2.0 -1.0
+    img = cv2.resize(img, (256, 32))
+    img = img / 255.0 * 2.0 - 1.0
     img = np.expand_dims(img, axis=0)
     img = np.expand_dims(img, axis=0)
     img = torch.tensor(img, dtype=torch.float)
@@ -48,24 +48,22 @@ def single_recognition(img,model_dir):
 
     model_for_predict = crnn().float()
     model_for_predict.load_state_dict(torch.load(model_dir))
-    # model_for_predict.load_state_dict(torch.load(model_dir, map_location='cpu'))
     model_for_predict.eval()
 
     y_pred_probMatrix = model_for_predict(img).squeeze()
 
-
     # Decode 阶段
-    decoder = GreedyCTCDecoder(labels = num2char_dict) # 使用的是最简单的贪婪算法
+    decoder = GreedyCTCDecoder(labels=num2char_dict)  # 使用的是最简单的贪婪算法
     y_pred_labels = decoder(y_pred_probMatrix)
+    print(len(y_pred_labels))
 
-    return y_pred_labels
-
+    return y_pred_labels, y_pred_probMatrix
 
 
 img = cv2.imread("test_images/7.jpeg", 0)
 IMG = Image(img)
-pred_labels = single_recognition(IMG.pos_img, model_dir)
-print("识别结果为：", pred_labels)
+pred_labels, probMatrix = single_recognition(IMG.pos_img, model_dir)
+print("识别结果为：", pred_labels, "长度为:", len(pred_labels))
 
 # img = cv2.imread("test1.jpg", 0)
 # pred_labels = single_recognition(img, model_dir)
